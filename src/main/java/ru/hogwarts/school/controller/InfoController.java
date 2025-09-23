778dff6e8ab7ee43ecc8d86303ea002e385083c9
bannikov_ilya <101068317+b4ntik@users.noreply.github.com>
2025-09-23 22:57:19 +0500

    fixByComments 1.1

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,17 +1,23 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.hogwarts.school.service.InfoService;
 
 @RestController
 public class InfoController {
-    @Autowired
-    public InfoService infoService;
+
+    @Value("${server.port}")
+    private int port;
+//    @Autowired
+//    public void setWebServerApplicationContext(WebServerApplicationContext context) {
+//        this.port = context.getWebServer().getPort();
+//    }
 
     @GetMapping("/port")
-    public Integer getServerPort(){
-        return infoService.getServerPort();
+    public Integer getServerPort() {
+        return port;
     }
 }
